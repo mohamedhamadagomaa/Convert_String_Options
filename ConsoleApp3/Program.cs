0f598af41a515bc862a0d13e7fc4b11aeeb2704c
Mohamed Hamada <moha2016226@gmail.com>
2024-10-14 04:39:48 -0700

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        static void Main(String[] args)
+        {
+            while (true)
+            {
+
+                Console.Write("PLease Enter Your String: ");
+                String input = Console.ReadLine();
+                Console.WriteLine("PLease Select an option");
+                Console.WriteLine("1. Convert To upper");
+                Console.WriteLine("2. Convert To lower");
+                Console.WriteLine("3. print the length");
+                String selectedOption = Console.ReadLine();
+                switch (selectedOption)
+                {
+                    default:
+                        Console.WriteLine("The Value is invalid");
+                        break;
+                    case "1":
+                        Console.WriteLine(input.ToUpper());
+                        break;
+                    case "2":
+                        Console.WriteLine(input.ToLower());
+                        break;
+                    case "3":
+                        Console.WriteLine(input.Length);
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
